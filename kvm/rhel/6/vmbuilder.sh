47179acf9343128c720ef0259adbffade637cfb5
Masahito Yoshida <masahito@axsh.net>
2012-09-13 15:10:33 +0900

    vmbuilder: add label using mountpoint to each disk.

--- kvm/rhel/6/vmbuilder.sh
+++ kvm/rhel/6/vmbuilder.sh
@@ -525,7 +525,7 @@ function mkfs2vm() {
       ${mkswap} ${part_filename}
       ;;
     *)
-      ${mkfs} -F -E lazy_itable_init=1 ${part_filename}
+      ${mkfs} -F -E lazy_itable_init=1 -L ${mountpoint} ${part_filename}
 
       # > This filesystem will be automatically checked every 37 mounts or 180 days, whichever comes first.
       # > Use tune2fs -c or -i to override.
