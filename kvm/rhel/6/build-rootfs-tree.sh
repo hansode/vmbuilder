065cbabdb001cd12b6eda97f99d7ee3e855c68eb
Masahito Yoshida <masahito@axsh.net>
2012-09-22 02:41:30 +0900

    bootstrap: move do_cleanup to functions area.

--- kvm/rhel/6/build-rootfs-tree.sh
+++ kvm/rhel/6/build-rootfs-tree.sh
@@ -89,6 +89,14 @@ function build_vers() {
   "
 }
 
+function do_cleanup() {
+  printf "[DEBUG] Caught signal\n"
+  umount -l ${chroot_dir}/proc
+  [ -d ${chroot_dir} ] && rm -rf ${chroot_dir}
+  [ -f ${repo} ] && rm -f ${repo}
+  printf "[DEBUG] Cleaned up\n"
+}
+
 function checkroot() {
   [[ $UID -ne 0 ]] && {
     echo "[ERROR] Must run as root." >&2
@@ -140,13 +148,6 @@ case ${yorn} in
   n|N|no|NO) exit 1;;
 esac
 
-function do_cleanup() {
-  printf "[DEBUG] Caught signal\n"
-  umount -l ${chroot_dir}/proc
-  [ -d ${chroot_dir} ] && rm -rf ${chroot_dir}
-  [ -f ${repo} ] && rm -f ${repo}
-  printf "[DEBUG] Cleaned up\n"
-}
 trap do_cleanup 1 2 3 15
 
 [ -d ${chroot_dir} ] && { echo "${chroot_dir} already exists." >&2; exit 1; }
