b9eb2c4846cf704c764378e7c686dc9545be62d7
Masahito Yoshida <masahito@axsh.net>
2012-10-14 22:30:49 +0900

    test/unit: add unit tests for mbr.

---
+++ kvm/rhel/6/test/unit/mbr/helper_shunit2.sh
@@ -0,0 +1,13 @@
+# -*-Shell-script-*-
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ../../../functions/mbr.sh
+
+## variables
+
+shunit2_file=../shunit2
---
+++ kvm/rhel/6/test/unit/mbr/t.rmmbr.sh
@@ -0,0 +1,32 @@
+# -*-Shell-script-*-
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ./helper_shunit2.sh
+
+## variables
+
+declare disk_filename=_disk.raw.$$
+
+## public functions
+
+function setUp() {
+  truncate -s 10m ${disk_filename}
+}
+
+function tearDown() {
+  rm -f ${disk_filename}
+}
+
+function test_rmmbr() {
+  rmmbr ${disk_filename} 2>/dev/null
+  assertEquals $? 0
+}
+
+## shunit2
+
+. ${shunit2_file}
