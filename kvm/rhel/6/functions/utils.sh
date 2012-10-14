47ecb0c70984a8119ae1bc05f838dd80b1569408
Masahito Yoshida <masahito@axsh.net>
2012-10-14 20:20:18 +0900

    utils: improve checkroot to use strict syntax.

--- kvm/rhel/6/functions/utils.sh
+++ kvm/rhel/6/functions/utils.sh
@@ -45,7 +45,7 @@ function checkroot() {
   #
   # Check if we're running as root, and bail out if we're not.
   #
-  [[ $UID -ne 0 ]] && {
+  [[ "${UID}" -ne 0 ]] && {
     echo "[ERROR] Must run as root." >&2
     return 1
   } || :
