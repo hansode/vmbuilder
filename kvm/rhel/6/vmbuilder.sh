6c390009d42364468d3f24d09082a613c54f3a97
Masahito Yoshida <masahito@axsh.net>
2012-10-14 14:25:42 +0900

    vmbuilder: move build_vmimage to functions.vm.

--- kvm/rhel/6/vmbuilder.sh
+++ kvm/rhel/6/vmbuilder.sh
@@ -10,12 +10,8 @@
 #  printf, awk, rm
 #
 # import:
-#   utils: checkroot, is_dev
-#   mbr: rmmbr
-#   disk: mkdisk, xptabinfo, mkptab, mapptab, mkfsdisk, unmapptab
-#   distro: build_chroot
-#   hypervisor: preflight_check_hypervisor, install_os
-#   vm: trap_vm
+#  utils: checkroot
+#  vm: build_vmimage
 #
 # memo:
 #
@@ -97,38 +93,6 @@ set -e
 function register_options() {
   debug=${debug:-}
   [[ -z "${debug}" ]] || set -x
-}
-
-## task
-
-function build_vmimage() {
-  preflight_check_hypervisor
-  [[ -d ${distro_dir} ]] || build_chroot ${distro_dir}
-
-  trap 'exit 1'  HUP INT PIPE QUIT TERM
-  trap "trap_vm ${raw} ${chroot_dir}" EXIT
-
-  is_dev ${raw} && {
-    rmmbr ${raw}
-  } || {
-    [[ -f ${raw} ]] && rm -f ${raw}
-    local totalsize=$(xptabinfo | awk 'BEGIN {sum = 0} {sum += $2} END {print sum}')
-    printf "[INFO] Creating disk image: \"%s\" of size: %dMB\n" ${raw} ${totalsize}
-    mkdisk ${raw} ${totalsize}
-  }
-  mkptab  ${raw}
-  is_dev ${raw} || {
-    printf "[INFO] Creating loop devices corresponding to the created partitions\n"
-    mapptab ${raw}
-  }
-  mkfsdisk ${raw}
-  install_os ${chroot_dir} ${distro_dir} ${raw} ${keepcache} ${execscript}
-  is_dev ${raw} || {
-    printf "[INFO] Deleting loop devices\n"
-    unmapptab ${raw}
-  }
-  printf "[INFO] Generated => %s\n" ${raw}
-  printf "[INFO] Complete!\n"
 }
 
 ### read-only variables
