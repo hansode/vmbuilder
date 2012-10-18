2d226eb93d60c719977b1a900e7006fb806cc3d7
Masahito Yoshida <masahito@axsh.net>
2012-10-18 21:20:06 +0900

    test/unit/mkptab: add zero/non-zero tests.

--- kvm/rhel/6/test/unit/02_disk/t.mkptab.sh
+++ kvm/rhel/6/test/unit/02_disk/t.mkptab.sh
@@ -12,15 +12,42 @@
 
 ## public functions
 
-function setUp() {
-  mkdisk ${disk_filename} ${totalsize} 2>/dev/null
-}
-
 function tearDown() {
   rm -f ${disk_filename}
 }
 
-function test_mkptab() {
+function test_mkptab_all_zero() {
+  rootsize=0 swapsize=0 optsize=0
+  totalsize=$((${rootsize} + ${swapsize} + ${optsize}))
+  mkdisk ${disk_filename} ${totalsize}
+
+  mkptab ${disk_filename}
+  assertNotEquals $? 0
+}
+
+function test_mkptab_root() {
+  rootsize=8 swapsize=0 optsize=0
+  totalsize=$((${rootsize} + ${swapsize} + ${optsize}))
+  mkdisk ${disk_filename} ${totalsize}
+
+  mkptab ${disk_filename}
+  assertEquals $? 0
+}
+
+function test_mkptab_root_swap() {
+  rootsize=8 swapsize=8 optsize=0
+  totalsize=$((${rootsize} + ${swapsize} + ${optsize}))
+  mkdisk ${disk_filename} ${totalsize}
+
+  mkptab ${disk_filename}
+  assertEquals $? 0
+}
+
+function test_mkptab_root_swap() {
+  rootsize=8 swapsize=8 optsize=8
+  totalsize=$((${rootsize} + ${swapsize} + ${optsize}))
+  mkdisk ${disk_filename} ${totalsize}
+
   mkptab ${disk_filename}
   assertEquals $? 0
 }
