8ab0703477f621d15313b7e4b55d66c43f85fd70
Masahito Yoshida <masahito@axsh.net>
2012-10-18 18:53:26 +0900

    test/unit: move shunit2 from test/unit/ to under test/.

--- kvm/rhel/6/test/unit/06_vm/helper_shunit2.sh
+++ kvm/rhel/6/test/unit/06_vm/helper_shunit2.sh
@@ -7,7 +7,7 @@
 ## system variables
 
 readonly abs_dirname=$(cd $(dirname $0) && pwd)
-readonly shunit2_file=${abs_dirname}/../shunit2
+readonly shunit2_file=${abs_dirname}/../../shunit2
 
 ## include files
 
