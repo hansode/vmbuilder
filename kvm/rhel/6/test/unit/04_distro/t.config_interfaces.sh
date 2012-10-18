25646ff5f8a745c344785036bec0c4dde0a2b940
Masahito Yoshida <masahito@axsh.net>
2012-10-18 22:21:32 +0900

    test/unit/distro: add all option tests.

--- kvm/rhel/6/test/unit/04_distro/t.config_interfaces.sh
+++ kvm/rhel/6/test/unit/04_distro/t.config_interfaces.sh
@@ -25,6 +25,8 @@ function test_config_interfaces_dhcp() {
   assertEquals $? 0
 }
 
+### set value
+
 function test_config_interfaces_static_ip() {
   local ip=192.0.2.10
   config_interfaces ${chroot_dir}
@@ -51,6 +53,38 @@ function test_config_interfaces_static_ip_net_mask_bcast() {
 
 function test_config_interfaces_static_ip_net_mask_bcast_gw() {
   local ip=192.0.2.10 net=192.0.2.0 mask=255.255.255.128 bcast=192.0.2.127 gw=192.0.2.1
+  config_interfaces ${chroot_dir}
+  assertEquals $? 0
+}
+
+### set empty
+
+function test_config_interfaces_ip_empty() {
+  local ip=
+  config_interfaces ${chroot_dir}
+  assertEquals $? 0
+}
+
+function test_config_interfaces_net_empty() {
+  local net=
+  config_interfaces ${chroot_dir}
+  assertEquals $? 0
+}
+
+function test_config_interfaces_mask_empty() {
+  local mask=
+  config_interfaces ${chroot_dir}
+  assertEquals $? 0
+}
+
+function test_config_interfaces_bcast_empty() {
+  local bcast=
+  config_interfaces ${chroot_dir}
+  assertEquals $? 0
+}
+
+function test_config_interfaces_gw_empty() {
+  local gw=
   config_interfaces ${chroot_dir}
   assertEquals $? 0
 }
