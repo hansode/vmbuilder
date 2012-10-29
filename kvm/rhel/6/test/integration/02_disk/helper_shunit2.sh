c8636c012c4f6103adffd00f237b3c9a40e2a2b1
Masahito Yoshida <masahito@axsh.net>
2012-10-29 21:06:19 +0900

    test/integration: add integration test for convert_disk.

---
+++ kvm/rhel/6/test/integration/02_disk/helper_shunit2.sh
@@ -0,0 +1,25 @@
+# -*-Shell-script-*-
+#
+# requires:
+#   bash
+#
+
+## system variables
+
+readonly abs_dirname=$(cd $(dirname $0) && pwd)
+readonly shunit2_file=${abs_dirname}/../../shunit2
+
+## include files
+
+. ${abs_dirname}/../../../functions/utils.sh
+. ${abs_dirname}/../../../functions/disk.sh
+
+## group variables
+
+declare chroot_dir=${abs_dirname}/_chroot.$$
+declare disk_filename=${abs_dirname}/_disk.$$.raw
+
+declare rootsize=8
+declare swapsize=8
+declare optsize=8
+declare totalsize=$((${rootsize} + ${swapsize} + ${optsize}))
---
+++ kvm/rhel/6/test/integration/02_disk/t.convert_disk.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ./helper_shunit2.sh
+
+## variables
+
+declare dest_filename=${disk_filename%%."$(get_suffix ${disk_filename})"}
+
+## public functions
+
+function setUp() {
+  mkdisk ${disk_filename} ${totalsize} 2>/dev/null
+  mkptab ${disk_filename}
+  mapptab ${disk_filename}
+  mkfsdisk ${disk_filename}
+}
+
+function tearDown() {
+  unmapptab ${disk_filename}
+  rm -f ${disk_filename}
+  rm -f ${dest_filename}.*
+}
+
+### no opts
+
+function test_convert_disk_no_opts() {
+  convert_disk
+  assertNotEquals $? 0
+}
+
+### set opts
+
+function test_convert_disk_filename() {
+  convert_disk ${disk_filename}
+}
+
+function test_convert_disk_filename_destdir() {
+  convert_disk ${disk_filename} `pwd`
+  assertEquals $? 0
+}
+
+#### raw -> vdi (virtualbox)
+
+function test_convert_disk_filename_destdir_destformat_vdi() {
+  convert_disk ${disk_filename} `pwd` vdi
+  assertEquals $? 0
+}
+
+#### raw -> vmdk (vmware)
+
+function test_convert_disk_filename_destdir_destformat_vmdk() {
+  convert_disk ${disk_filename} `pwd` vmdk
+  assertEquals $? 0
+}
+
+## shunit2
+
+. ${shunit2_file}
