1053e68018c10619ce39efa7fc023712aed6f613
Masahito Yoshida <masahito@axsh.net>
2012-10-29 14:33:10 +0900

    misc: add raw2vdi.sh which converts raw to vdi.

---
+++ kvm/rhel/6/misc/raw2vdi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+
+### read-only variables
+
+readonly abs_dirname=$(cd $(dirname $0) && pwd)
+
+### include files
+
+. ${abs_dirname}/../functions/utils.sh
+. ${abs_dirname}/../functions/disk.sh
+
+### prepare
+
+# disk_filename=$1
+convert_disk ${1}
