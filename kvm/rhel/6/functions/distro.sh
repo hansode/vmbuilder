fcec545521916f6ec2481b993771c4719485bc0c
Masahito Yoshida <masahito@axsh.net>
2012-10-30 11:35:47 +0900

    distro: plugin -> driver.

--- kvm/rhel/6/functions/distro.sh
+++ kvm/rhel/6/functions/distro.sh
@@ -35,10 +35,10 @@ function add_option_distro() {
 
   distro_name=$(get_normalized_distro_name ${distro_name})
 
-  local distro_plugin_name="${distro_name}$(get_distro_major_ver ${distro_ver})"
-  case "${distro_plugin_name}" in
+  local distro_driver_name="${distro_name}$(get_distro_major_ver ${distro_ver})"
+  case "${distro_driver_name}" in
   centos6|sl6)
-    load_plugin_distro ${distro_plugin_name}
+    load_driver_distro ${distro_driver_name}
     ;;
   *)
     echo "no mutch distro" >&2
@@ -47,15 +47,15 @@ function add_option_distro() {
   esac
 }
 
-function load_plugin_distro() {
-  local distro_plugin_name=$1
-  [[ -n "${distro_plugin_name}" ]] || { echo "[ERROR] Invalid argument: distro_plugin_name:${distro_plugin_name} (distro:${LINENO})" >&2; return 1; }
+function load_driver_distro() {
+  local distro_driver_name=$1
+  [[ -n "${distro_driver_name}" ]] || { echo "[ERROR] Invalid argument: distro_driver_name:${distro_driver_name} (distro:${LINENO})" >&2; return 1; }
 
-  local distro_plugin_path=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/distro/${distro_plugin_name}.sh
-  [[ -f "${distro_plugin_path}" ]] || { echo "[ERROR] no such distro plugin: ${distro_plugin_path} (distro:${LINENO})" >&2; return 1; }
+  local distro_driver_path=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/distro/${distro_driver_name}.sh
+  [[ -f "${distro_driver_path}" ]] || { echo "[ERROR] no such distro plugin: ${distro_driver_path} (distro:${LINENO})" >&2; return 1; }
 
-  . ${distro_plugin_path}
-  add_option_distro_${distro_plugin_name}
+  . ${distro_driver_path}
+  add_option_distro_${distro_driver_name}
 }
 
 function get_normalized_distro_name() {
--- kvm/rhel/6/test/unit/04_distro/t.load_plugin_distro.sh
+++ kvm/rhel/6/test/unit/04_distro/t.load_plugin_distro.sh
@@ -12,18 +12,18 @@
 
 ## public functions
 
-function test_load_plugin_distro_centos6() {
-  load_plugin_distro centos6
+function test_load_driver_distro_centos6() {
+  load_driver_distro centos6
   assertEquals $? 0
 }
 
-function test_load_plugin_distro_sl6() {
-  load_plugin_distro centos6
+function test_load_driver_distro_sl6() {
+  load_driver_distro centos6
   assertEquals $? 0
 }
 
-function test_load_plugin_distro_unknown() {
-  load_plugin_distro unknown
+function test_load_driver_distro_unknown() {
+  load_driver_distro unknown
   assertNotEquals $? 0
 }
 
