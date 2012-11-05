926ed508caa93122028e283a90cf09b8c0956e38
Masahito Yoshida <masahito@axsh.net>
2012-11-05 20:29:59 +0900

    distro/centos5: set a default minor version.

--- kvm/rhel/6/functions/distro/centos5.sh
+++ kvm/rhel/6/functions/distro/centos5.sh
@@ -13,6 +13,10 @@
 function add_option_distro_centos5() {
   load_distro_driver rhel5
 
+  case ${distro_ver} in
+  5) distro_ver=5.8 ;;
+  esac
+
   baseurl=${baseurl:-http://ftp.riken.go.jp/pub/Linux/centos/${distro_ver}/os/${basearch}}
   gpgkey=${gpgkey:-${baseurl}/RPM-GPG-KEY-CentOS-5}
 }
