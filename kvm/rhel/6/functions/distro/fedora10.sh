acd399f3e960fb67370a7ad94291388bf769a974
Masahito Yoshida <masahito@axsh.net>
2012-11-06 23:04:26 +0900

    distro: add comments.

--- kvm/rhel/6/functions/distro/fedora10.sh
+++ kvm/rhel/6/functions/distro/fedora10.sh
@@ -13,5 +13,6 @@
 function add_option_distro_fedora10() {
   load_distro_driver fedora9
 
+  # *** should be placed after load_distro_driver to re-define following variables. ***
   gpgkey="${baseurl}/RPM-GPG-KEY-fedora ${baseurl}/RPM-GPG-KEY-fedora-${distro_ver}-primary ${baseurl}/RPM-GPG-KEY-fedora-${basearch}"
 }
--- kvm/rhel/6/functions/distro/fedora12.sh
+++ kvm/rhel/6/functions/distro/fedora12.sh
@@ -13,6 +13,7 @@
 function add_option_distro_fedora12() {
   load_distro_driver fedora11
 
+  # *** should be placed after load_distro_driver to re-define following variables. ***
   preferred_filesystem=ext4
   preferred_initrd=initramfs
 }
