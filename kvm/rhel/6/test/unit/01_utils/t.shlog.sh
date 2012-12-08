a4b4a86190a0a7babfb9eca092e8d70f64703f1d
Masahito Yoshida <masahito@axsh.net>
2012-12-08 10:01:46 +0900

    utils,misc/kvm-ctl.sh: move shlog() to utils.

---
+++ kvm/rhel/6/test/unit/01_utils/t.shlog.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ./helper_shunit2.sh
+
+## variables
+
+declare chroot_dir=${abs_dirname}/_chroot.$$
+
+## public functions
+
+function setUp() {
+  mkdir -p ${chroot_dir}
+
+  function chroot() { echo chroot $*; }
+}
+
+function tearDown() {
+  rm -rf ${chroot_dir}
+}
+
+test_shlog_available_command() {
+  assertEquals \
+   "`shlog echo hello`" \
+        "\$ echo hello
+hello"
+}
+
+test_shlog_unavailable_command() {
+  assertNotEquals \
+   "`shlog typo hello 2>/dev/null`" \
+        "\$ typo hello
+hello"
+}
+
+## shunit2
+
+. ${shunit2_file}
