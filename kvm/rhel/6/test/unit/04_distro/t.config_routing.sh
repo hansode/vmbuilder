8aed9f30aad89a1de8c812bbb199657957071223
Masahito Yoshida <masahito@axsh.net>
2012-12-13 14:04:44 +0900

    distro,test: fix routetab format.

--- kvm/rhel/6/test/unit/04_distro/t.config_routing.sh
+++ kvm/rhel/6/test/unit/04_distro/t.config_routing.sh
@@ -17,21 +17,21 @@ declare routetab_file=${abs_dirname}/routetab.$$
 ## public functions
 
 function setUp() {
-  mkdir -p ${chroot_dir}
+  mkdir -p ${chroot_dir}/etc/sysconfig/network-scripts
 
   cat <<EOS > ${routetab_file}
 # eth0
-ifname=eth0 net=10.0.2.0 mask=255.255.255.0   gw=10.0.2.254
-ifname=eth0 net=10.0.3.0 mask=255.255.255.128 gw=10.0.3.254
+ifname=eth0 cidr=10.0.2.0/24 gw=10.0.2.254
+ifname=eth0 cidr=10.0.3.0/25 gw=10.0.3.254
 
 # eth1
-ifname=eth1 net=10.1.4.0 mask=255.255.255.192 gw=10.1.4.254
+ifname=eth1 cidr=10.1.4.0/26 gw=10.1.4.254
 
 # eth2
-ifname=eth2 net=10.2.5.0 mask=255.255.0.0 gw=10.0.5.1
+ifname=eth2 cidr=10.2.5.0/16 gw=10.0.5.1
 EOS
 
-  function install_routing() { echo install_routing $*; }
+ #function install_routing() { echo install_routing $*; }
 }
 
 function tearDown() {
