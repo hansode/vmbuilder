51f47472dc8e99e09c63ca814670e12e21910878
Masahito Yoshida <masahito@axsh.net>
2012-12-13 14:28:45 +0900

    distro,test: add tests for config_routing().

---
+++ kvm/rhel/6/test/integration/04_distro/t.config_routing.file.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ./helper_shunit2.sh
+
+## variables
+
+declare routetab=${abs_dirname}/../../../routetab.txt.example
+
+## public functions
+
+function setUp() {
+  mkdir -p ${chroot_dir}/etc/sysconfig/network-scripts
+}
+
+function tearDown() {
+  rm -f ${routetab_file}
+  rm -rf ${chroot_dir}
+}
+
+function test_config_routing() {
+  config_routing ${chroot_dir}
+}
+
+## shunit2
+
+. ${shunit2_file}
---
+++ kvm/rhel/6/test/unit/04_distro/t.config_routing.file.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. ./helper_shunit2.sh
+
+## variables
+
+declare routetab=${abs_dirname}/../../../routetab.txt.example
+
+## public functions
+
+function setUp() {
+  mkdir -p ${chroot_dir}
+
+  function install_routing() { echo install_routing $*; }
+}
+
+function tearDown() {
+  rm -f ${routetab_file}
+  rm -rf ${chroot_dir}
+}
+
+function test_config_routing() {
+  config_routing ${chroot_dir}
+}
+
+## shunit2
+
+. ${shunit2_file}
