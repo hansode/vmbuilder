511dbf519d7bbf478f722fcee92aa7f0db7e6d3b
Masahito Yoshida <masahito@axsh.net>
2012-12-19 01:11:09 +0900

    disk,test: replace integration test with unit test.

--- kvm/rhel/6/test/unit/02_disk/t.mkdisk.sh
+++ kvm/rhel/6/test/unit/02_disk/t.mkdisk.sh
@@ -13,12 +13,16 @@
 
 ## public functions
 
+function setUp() {
+  function truncate() { :; }
+}
+
 function tearDown() {
   rm -f ${disk_filename}
 }
 
 function test_mkdisk_size_zero() {
-  mkdisk ${disk_filename} 0
+  mkdisk ${disk_filename} 0 2>/dev/null
   assertNotEquals $? 0
 }
 
