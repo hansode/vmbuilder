4800505e2c4e05fae26baa573aa60834fce82021
Masahito Yoshida <masahito@axsh.net>
2012-12-19 02:32:40 +0900

    disk,test: replace integration test with unit test.

--- kvm/rhel/6/test/unit/02_disk/t.unmapptab.sh
+++ kvm/rhel/6/test/unit/02_disk/t.unmapptab.sh
@@ -14,9 +14,18 @@
 ## public functions
 
 function setUp() {
-  mkdisk ${disk_filename} $(sum_disksize) 2>/dev/null
-  mkptab ${disk_filename}
-  mapptab ${disk_filename}
+  touch ${disk_filename}
+
+  function checkroot() { :; }
+  function kpartx() { :; }
+  function dmsetup() { :; }
+  function losetup() { :; }
+  function mapped_lodev() { :; }
+  function lsdevmap() { cat <<-EOS
+	loop0p1
+	loop0p2
+	EOS
+  }
 }
 
 function tearDown() {
@@ -24,7 +33,7 @@ function tearDown() {
 }
 
 function test_unmapptab() {
-  unmapptab ${disk_filename}
+  unmapptab ${disk_filename} >/dev/null
   assertEquals $? 0
 }
 
