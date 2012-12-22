854eb970a33b6e17733d125415a9a29c592467ed
Masahito Yoshida <masahito@axsh.net>
2012-12-22 11:50:14 +0900

    misc: add devproc-mounter.sh.

---
+++ kvm/rhel/6/misc/devproc-mounter.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# description:
+#  Mount/Umount a virtual filesystems (/dev, /proc, /sys)
+#
+# requires:
+#  bash
+#  dirname, pwd
+#  sed, head
+#  egrep
+#
+# import:
+#  utils: extract_args
+#  disk: add_option_disk
+#  hypervisor: mount_dev, mount_proc, mount_sys, umount_nonroot
+#
+# usage:
+#
+#  $0 COMMAND --config-path=/path/to/config
+#
+set -e
+
+## private functions
+
+function register_options() {
+  debug=${debug:-}
+  [[ -z "${debug}" ]] || set -x
+
+  config_path=${config_path:-}
+  mntpnt_path=${mntpnt_path:-$(pwd)/mnt}
+}
+
+function nonroot_mounter() {
+  local cmd=$1
+  [[ -n "${cmd}" ]] || { echo "[ERROR] Invalid argument: cmd:${cmd} ($(basename ${BASH_SOURCE[0]}):${LINENO})" >&2; return 1; }
+  checkroot || return 1
+
+  case "${cmd}" in
+  mount|umount)
+    [[ -d "${mntpnt_path}" ]] || { echo "[ERROR] no such directory: ${mntpnt_path} ($(basename ${BASH_SOURCE[0]}):${LINENO})" >&2; return 1; }
+
+    case "${cmd}" in
+    mount)
+      mount_dev  ${mntpnt_path}
+      mount_proc ${mntpnt_path}
+      mount_sys  ${mntpnt_path}
+      ;;
+    umount)
+      umount_nonroot ${mntpnt_path}
+      ;;
+    esac
+
+    ;;
+  ls)
+    mount | egrep ${mntpnt_path}
+    ;;
+  *)
+    echo "[ERROR] no such command: ${cmd} ($(basename ${BASH_SOURCE[0]}):${LINENO})" >&2
+    return 1
+    ;;
+  esac
+}
+
+### read-only variables
+
+readonly abs_dirname=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+
+### include files
+
+. ${abs_dirname}/../functions/utils.sh
+. ${abs_dirname}/../functions/disk.sh
+. ${abs_dirname}/../functions/hypervisor.sh
+
+### prepare
+
+extract_args $*
+
+### main
+
+declare cmd="$(echo ${CMD_ARGS} | sed "s, ,\n,g" | head -1)"
+
+[[ -f "${config_path}" ]] && load_config ${config_path} || :
+register_options
+add_option_disk
+nonroot_mounter ${cmd}
