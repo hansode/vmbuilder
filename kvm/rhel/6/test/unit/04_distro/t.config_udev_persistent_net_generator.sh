db8662652dd33927639449e8cda3bb315740c6d4
Masahito Yoshida <masahito@axsh.net>
2012-12-23 20:03:38 +0900

    distro: add config_udev_persistent_net_generator().

---
+++ kvm/rhel/6/test/unit/04_distro/t.config_udev_persistent_net_generator.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/helper_shunit2.sh
+
+## variables
+
+## public functions
+
+function setUp() {
+  mkdir -p ${chroot_dir}/lib/udev/rules.d
+  cat <<-EOS > ${chroot_dir}/lib/udev/rules.d/75-persistent-net-generator.rules
+	ENV{MATCHADDR}=="00:00:00:00:00:00", ENV{MATCHADDR}=""
+EOS
+}
+
+function tearDown() {
+  rm -rf ${chroot_dir}
+}
+
+function test_config_udev_persistent_net_generator_file_not_found() {
+  rm ${chroot_dir}/lib/udev/rules.d/75-persistent-net-generator.rules
+
+  config_udev_persistent_net_generator ${chroot_dir}
+  assertNotEquals $? 0
+}
+
+function test_config_udev_persistent_net_generator() {
+  config_udev_persistent_net_generator ${chroot_dir}
+
+  egrep '^ENV{MATCHADDR}=="00:00:00:00:00:00", ENV{MATCHADDR}=""' -6 ${chroot_dir}/lib/udev/rules.d/75-persistent-net-generator.rules
+  assertEquals $? 0
+}
+
+## shunit2
+
+. ${shunit2_file}
