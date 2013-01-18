33653b665de74377ae3dafebe2fce0a93a051630
Masahito Yoshida <masahito@axsh.net>
2013-01-18 18:30:54 +0900

    distro: support multi "copy".

--- kvm/rhel/6/test/unit/05_hypervisor/t.install_os.sh
+++ kvm/rhel/6/test/unit/05_hypervisor/t.install_os.sh
@@ -43,7 +43,7 @@ function setUp() {
   function install_addedpkgs() { echo install_addedpkgs $*; }
   function convert_rpmdb_hash() { echo convert_rpmdb_hash $*; }
   function clean_packages() { echo clean_packages $*; }
-  function run_copy()       { echo run_copy       $*; }
+  function run_xcopy()      { echo run_xcopy      $*; }
   function run_execscript() { echo run_execscript $*; }
   function install_firstboot() { echo install_firstboot $*; }
   function install_firstlogin() { echo install_firstlogin $*; }
