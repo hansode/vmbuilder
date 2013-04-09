97b0007745ab326ad1a1d645a04745d7a94a4637
Masahito Yoshida <masahito@axsh.net>
2013-04-09 10:36:40 +0900

    hypervisor/kvm: enable to define nic driver with "nic_driver" parameter.

--- kvm/rhel/6/test/unit/05_hypervisor/kvm/t.build_vif_opt.sh
+++ kvm/rhel/6/test/unit/05_hypervisor/kvm/t.build_vif_opt.sh
@@ -22,6 +22,18 @@ function test_build_vif_opt_set_vif_num() {
   assertEquals "$(build_vif_opt | wc -l)" ${vif_num}
 }
 
+function test_build_vif_opt_defined_nic_driver_e1000() {
+  local nic_driver=e1000
+  build_vif_opt | egrep -q -- "-device ${nic_driver},"
+  assertEquals $? 0
+}
+
+function test_build_vif_opt_defined_nic_driver_virtio() {
+  local nic_driver=virtio-net-pci
+  build_vif_opt | egrep -q -- "-device ${nic_driver},"
+  assertEquals $? 0
+}
+
 ## shunit2
 
 . ${shunit2_file}
