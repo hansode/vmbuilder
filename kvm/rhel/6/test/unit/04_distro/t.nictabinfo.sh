fca92bd5f054d32b73715a78fb0548f64b77bd5d
Masahito Yoshida <masahito@axsh.net>
2013-04-28 11:56:53 +0900

    distro: fix multi line dns value.

--- kvm/rhel/6/test/unit/04_distro/t.nictabinfo.sh
+++ kvm/rhel/6/test/unit/04_distro/t.nictabinfo.sh
@@ -43,6 +43,21 @@ function test_nictabinfo_set_args() {
   assertEquals "$(nictabinfo | egrep -w "^ifname=${ifname} ip=${ip} mask=${mask} net=${net} bcast=${bcast} gw=${gw} dns=\"${dns}\" onboot= iftype=ethernet")" "ifname=${ifname} ip=${ip} mask=${mask} net=${net} bcast=${bcast} gw=${gw} dns=\"${dns}\" onboot= iftype=ethernet"
 }
 
+function test_nictabinfo_set_args_multi_line_dns() {
+  local ifname=eth0
+  local ip=192.0.2.10
+  local mask=255.255.255.0
+  local net=192.0.2.0
+  local bcast=192.0.2.255
+  local gw=192.0.2.1
+  local dns="
+ 8.8.4.4
+ 8.8.8.8
+"
+
+  assertEquals "$(nictabinfo | egrep -w "^ifname=${ifname} ip=${ip} mask=${mask} net=${net} bcast=${bcast} gw=${gw} dns=\"$(echo ${dns})\" onboot= iftype=ethernet")" "ifname=${ifname} ip=${ip} mask=${mask} net=${net} bcast=${bcast} gw=${gw} dns=\"$(echo ${dns})\" onboot= iftype=ethernet"
+}
+
 ## shunit2
 
 . ${shunit2_file}
