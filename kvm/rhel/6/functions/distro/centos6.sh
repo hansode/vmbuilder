cb6002cbc959834fe11fbd8708a7765d495edac0
Masahito Yoshida <masahito@axsh.net>
2014-09-04 17:56:25 +0900

    distro: use vault.centos.org instead of ftp.riken.co.jp.

--- kvm/rhel/6/functions/distro/centos6.sh
+++ kvm/rhel/6/functions/distro/centos6.sh
@@ -14,6 +14,6 @@ function add_option_distro_centos6() {
   load_distro_driver rhel6
   load_distro_driver centos
 
-  baseurl=${baseurl:-http://ftp.riken.go.jp/pub/Linux/centos/${distro_ver}/os/${basearch}}
+  baseurl=${baseurl:-http://vault.centos.org/${distro_ver}/os/${basearch}}
   gpgkey=${gpgkey:-${baseurl}/RPM-GPG-KEY-CentOS-6}
 }
