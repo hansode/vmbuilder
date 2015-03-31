7ac6f6d67c49dd01e84c2de53c6c846568653bb3
Masahito Yoshida <masahito@axsh.net>
2015-03-31 21:09:33 +0900

    update centos7 latest version.

--- kvm/rhel/6/functions/distro/rhel7.sh
+++ kvm/rhel/6/functions/distro/rhel7.sh
@@ -13,10 +13,10 @@
 function add_option_distro_rhel7() {
   load_distro_driver rhel6
 
-  distro_ver_latest=7.0.1406
+  distro_ver_latest=7.1.1503
 
   case ${distro_ver} in
-  7|7.0) distro_ver=${distro_ver_latest} ;;
+  7|7.1) distro_ver=${distro_ver_latest} ;;
   esac
 
   preferred_grub=grub2
